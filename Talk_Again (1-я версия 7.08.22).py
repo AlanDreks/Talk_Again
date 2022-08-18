ccb91e020f0571c14cfa5cba349d90afa63dec49
alan <alandreks@inbox.ru>
2022-08-19 00:55:48 +0700

    overwriting the main function

--- Talk_Again (1-я версия 7.08.22).py
+++ Talk_Again (1-я версия 7.08.22).py
@@ -1,10 +1,38 @@
 import os
 # import random
 
+alphabet_and_syllables_data = ('а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т',
+                               'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ы', 'э', 'ю', 'аб', 'яб', 'уб', 'юб', 'об', 'ёб',
+                               'эб', 'еб', 'ыб', 'иб', 'ав', 'яв', 'ув', 'юв', 'ов', 'ёв', 'эв', 'ев', 'ыв', 'ив', 'аг',
+                               'яг', 'уг', 'юг', 'ог', 'ёг', 'эг', 'ег', 'ыг', 'иг', 'ад', 'яд', 'уд', 'юд', 'од', 'ёд',
+                               'эд', 'ед', 'ыд', 'ид', 'аж', 'яж', 'уж', 'юж', 'ож', 'ёж', 'эж', 'еж', 'ыж', 'иж', 'аз',
+                               'яз', 'уз', 'юз', 'оз', 'ёз', 'эз', 'ез', 'ыз', 'из', 'ай', 'яй', 'уй', 'ой', 'ёй', 'эй',
+                               'ей', 'ый', 'ий', 'ак', 'як', 'ук', 'юк', 'ок', 'ёк', 'эк', 'ек', 'ык', 'ик', 'ал', 'ял',
+                               'ул', 'юл', 'ол', 'ёл', 'эл', 'ел', 'ыл', 'ил', 'ам', 'ям', 'ум', 'юм', 'ом', 'ём', 'эм',
+                               'ем', 'ым', 'им', 'ан', 'ян', 'ун', 'юн', 'он', 'ён', 'эн', 'ен', 'ын', 'ин', 'ап', 'яп',
+                               'уп', 'юп', 'оп', 'ёп', 'эп', 'еп', 'ып', 'ип', 'ар', 'яр', 'ур', 'юр', 'ор', 'ёр', 'эр',
+                               'ер', 'ыр', 'ир', 'ас', 'яс', 'ус', 'юс', 'ос', 'ёс', 'эс', 'ес', 'ыс', 'ис', 'ат', 'ят',
+                               'ут', 'ют', 'от', 'ёт', 'эт', 'ет', 'ыт', 'ит', 'аф', 'яф', 'уф', 'юф', 'оф', 'ёф', 'эф',
+                               'еф', 'ыф', 'иф', 'ах', 'ях', 'ух', 'юх', 'ох', 'ёх', 'эх', 'ех', 'ых', 'их', 'ац', 'яц',
+                               'уц', 'юц', 'оц', 'ёц', 'эц', 'ец', 'ыц', 'иц', 'аш', 'яш', 'уш', 'юш', 'ош', 'ёш', 'эш',
+                               'еш', 'ыш', 'иш', 'ащ', 'ящ', 'ущ', 'ющ', 'ощ', 'ёщ', 'эщ', 'ещ', 'ыщ', 'ищ', 'ба', 'бя',
+                               'бу', 'бю', 'бо', 'бё', 'бэ', 'бе', 'бы', 'би', 'ва', 'вя', 'ву', 'вю', 'во', 'вё', 'вэ',
+                               'ве', 'вы', 'ви', 'га', 'гя', 'гу', 'гю', 'го', 'гё', 'гэ', 'ге', 'гы', 'ги', 'да', 'дя',
+                               'ду', 'дю', 'до', 'дё', 'дэ', 'де', 'ды', 'ди', 'жа', 'жя', 'жу', 'жю', 'жо', 'жё', 'жэ',
+                               'же', 'жи', 'за', 'зя', 'зу', 'зю', 'зо', 'зё', 'зэ', 'зе', 'зы', 'зи', 'ка', 'кя', 'ку',
+                               'кю', 'ко', 'кё', 'кэ', 'ке', 'кы', 'ки', 'ла', 'ля', 'лу', 'лю', 'ло', 'лё', 'лэ', 'ле',
+                               'лы', 'ли', 'ма', 'мя', 'му', 'мю', 'мо', 'мё', 'мэ', 'ме', 'мы', 'ми', 'на', 'ня', 'ну',
+                               'ню', 'но', 'нё', 'нэ', 'не', 'ны', 'ни', 'па', 'пя', 'пу', 'пю', 'по', 'пё', 'пэ', 'пе',
+                               'пы', 'пи', 'ра', 'ря', 'ру', 'рю', 'ро', 'рё', 'рэ', 'ре', 'ры', 'ри', 'са', 'ся', 'су',
+                               'сю', 'со', 'сё', 'сэ', 'се', 'сы', 'си', 'та', 'тя', 'ту', 'тю', 'то', 'тё', 'тэ', 'те',
+                               'ты', 'ти', 'фа', 'фу', 'фо', 'фё', 'фэ', 'фе', 'фы', 'фи', 'ха', 'хя', 'ху', 'хо', 'хё',
+                               'хэ', 'хе', 'хы', 'хи', 'ца', 'цу', 'цо', 'це', 'цы', 'ци', 'ша', 'шу', 'шо', 'шэ', 'ше',
+                               'ши', 'ща', 'щу', 'що', 'ще', 'щи')
+
 
 def viewing(file_name_func, date_today_func):
     alphabet_file_statistic = open(
-        os.path.join('/media/alan/5C546A3B546A1852/Talk_Again/', file_name_func)
+        os.path.join('/media/alan/5C546A3B546A1852/Talk_Again/Talk_Again_Project/', file_name_func)
         , 'r', encoding='utf-8')
     for letter in alphabet_file_statistic:
         if letter != '\n':
@@ -12,50 +40,41 @@ def viewing(file_name_func, date_today_func):
     alphabet_file_statistic.close()
 
 
-def choice_user(file_name_func, date_today_func):
-    test_file = open(
-        os.path.join('/media/alan/5C546A3B546A1852/Talk_Again/', file_name_func)
-        , 'r', encoding='utf-8')
-    dict_all_parts = dict()
-    for part in test_file:
-        if part != '\n':
-            all_parts = part.split()
-            dict_all_parts[all_parts[0]] = all_parts[1]
-    test_file.close()
-
-    average_condition = open(
-        os.path.join('/media/alan/5C546A3B546A1852/Talk_Again/', file_name_func)
-        , 'w', encoding='utf-8')
-    average_condition.write('               {}'.format(date_today_func))
-    average_condition.close()
-
-    test_file = open(
-        os.path.join('/media/alan/5C546A3B546A1852/Talk_Again/', file_name_func)
-        , 'a', encoding='utf-8')
-    for key_part, val_part in dict_all_parts.items():
-        print('\n' + key_part)
-        user_index_part = int(input('\nКак хорошо прошло повторение(от 1 до 5): '))
-        if val_part == '0':
-            index_part = str(user_index_part)
-        else:
-            index_part = (float(val_part) + user_index_part) / 2
-        letter = key_part + '          ' + str(index_part)
-        test_file.write('\n\n   ' + letter)
-    test_file.close()
+def choice_user(date_today_func, alphabet_and_syllables_data_func):
+    dict_today_home_work = dict()
+    dict_today_home_work['дата'] = date_today_func
+    user_index_part = ''
+    for part_syllables in alphabet_and_syllables_data_func:
+        if user_index_part == 'stop':
+            break
+        print(part_syllables)
+        while True:
+            user_index_part = input('\nКак хорошо прошло повторение(от 1 до 5): ')
+            if user_index_part in ['1', '2', '3', '4', '5']:
+                dict_today_home_work[part_syllables] = user_index_part
+                break
+            elif user_index_part not in ['1', '2', '3', '4', '5', 'stop']:
+                print('\nВведите оценку или стоп-слово')
+                continue
+            elif user_index_part == 'stop':
+                for part_syllables_2 in alphabet_and_syllables_data_func:
+                    if part_syllables_2 not in dict_today_home_work.keys():
+                        dict_today_home_work[part_syllables_2] = 0
+                break
+    average_condition = open('alphabet_syllables_list_file_statistic.txt', 'a', encoding='utf-8')
+    average_condition.write(str(dict_today_home_work))
 
 
 def user_choice_test(choice_test_alpha_syl_word):
     if choice_test_alpha_syl_word == '1':
-        return 'alphabet_file_statistic.txt'
+        return 'alphabet_syllables_list_file_statistic.txt'
     elif choice_test_alpha_syl_word == '2':
-        return 'syllables_list_file_statistic.txt'
-    elif choice_test_alpha_syl_word == '3':
         return 'finish_list_words.txt'
 
 
 def make_words(file_name_make_words, date_today_func):
     test_file_make_words = open(
-        os.path.join('/media/alan/5C546A3B546A1852/Talk_Again/', file_name_make_words)
+        os.path.join('/media/alan/5C546A3B546A1852/Talk_Again/Talk_Again_Project/', file_name_make_words)
         , 'r', encoding='utf-8')
     dict_syllable = {}
     for syllable in test_file_make_words:
@@ -70,7 +89,8 @@ def make_words(file_name_make_words, date_today_func):
         step += 1
         for syl_key_2, syl_val_2 in dict_syllable.items():
             dal_file_write_word = open(
-                os.path.join('/media/alan/5C546A3B546A1852/Talk_Again/', 'words_list_file_statistic.txt')
+                os.path.join(
+                    '/media/alan/5C546A3B546A1852/Talk_Again/Talk_Again_Project/words_list_file_statistic.txt')
                 , 'r', encoding='utf-8')
             for dal_word in dal_file_write_word:
                 dal_word_fin = dal_word[:-1].lower()
@@ -78,13 +98,13 @@ def make_words(file_name_make_words, date_today_func):
                 if dal_word_fin == syl_key_1_2:
                     print(dal_word_fin)
                     test_file_write_word = open(
-                        os.path.join('/media/alan/5C546A3B546A1852/Talk_Again/', 'finish_list_words.txt')
+                        os.path.join('/media/alan/5C546A3B546A1852/Talk_Again/Talk_Again_Project/finish_list_words.txt')
                         , 'a', encoding='utf-8')
                     test_file_write_word.write('\n' + syl_key + syl_key_2)
                     test_file_write_word.close()
 
 
-date_today = input('Введите дату формата хх.хх.хх :')
+date_today = input('Введите дату формата хх.хх.хх: ')
 
 # Choice start
 while True:
@@ -106,17 +126,21 @@ if user_answer == '1':
     # 3 - может повторить четко
     # 2 - может повторить с дефектами
     # 1 - не может повторить
-    choice_test = input('Выберите тест(1 - буквы, 2 - слоги, 3 - слова): ')
-    user_choice_test = user_choice_test(choice_test)
-    choice_user(user_choice_test, date_today)
+    while True:
+        choice_test = input('Выберите тест(1 - слоги, 2 - слова): ')
+        if choice_test == '1' or choice_test == '2':
+            user_choice_test = user_choice_test(choice_test)
+            choice_user(date_today, alphabet_and_syllables_data)
+            break
+        else:
+            print('Неправильный ввод! Выберите 1 или 2!')
 
-
-# 2. Просмотреть статистику по буквам и слогам(Закончено)
+# 2. Просмотреть статистику по буквам и слогам
 if user_answer == '2':
-    file_name = 'syllables_list_file_statistic.txt'
+    file_name = 'alphabet_syllables_list_file_statistic.txt'
     viewing(file_name, date_today)
 
-# 3. Просмотреть статистику по списку слов(Закончено)
+# 3. Просмотреть статистику по списку слов
 if user_answer == '3':
     file_name = 'finish_list_words.txt'
     viewing(file_name, date_today)
